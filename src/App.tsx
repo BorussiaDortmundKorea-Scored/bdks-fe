35f36055ad8fe0fdc77c8e77af3058cdd5b1d534
youngduck <dudejr5839@naver.com>
2025-07-10 15:41:03 +0900

    chore: 기본라우터설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Router from "./shared/router/Router";
 
 export const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="text-red-500">어플리케이션</div>
+      <Router />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
---
+++ src/shared/pages/rating/player-rating-page.tsx
@@ -0,0 +1,5 @@
+const PlayerRatingPage = () => {
+  return <div className="min-h-screen bg-gray-100">선수평가 페이지</div>;
+};
+
+export default PlayerRatingPage;
