3cd3f79badf1cb6fa2d2a61efaf711fe60792e57
youngduck <dudejr5839@naver.com>
2025-07-10 17:12:19 +0900

    refactor: 로그인 axios 인스턴스 네이밍 직관적으로 수정

--- src/domains/animals/api/animalApi.ts
+++ src/domains/animals/api/animalApi.ts
@@ -1,9 +1,9 @@
 // domains/animal/api/animalApi.ts
 import { axiosPublic } from "../../../shared/api/axiosPublic";
-import { axiosPrivate } from "../../../shared/api/axiosPrivate";
+import { axiosAuth } from "../../../shared/api/axiosAuth";
 
 // 공개 데이터
 export const getAnimals = () => axiosPublic.get("/animal");
 
 // 로그인 필요 데이터
-export const getMyProfile = () => axiosPrivate.get("/profile");
+export const getMyProfile = () => axiosAuth.get("/profile");
--- src/shared/api/axiosAuth.ts
+++ src/shared/api/axiosAuth.ts
@@ -1,10 +1,10 @@
-// src/shared/api/axiosPrivate.ts
+// src/shared/api/axiosAuth.ts
 import axios from "axios";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const axiosPrivate = axios.create({
+export const axiosAuth = axios.create({
   baseURL: `${supabaseUrl}/rest/v1`,
   headers: {
     apikey: supabaseKey,
@@ -13,7 +13,7 @@ export const axiosPrivate = axios.create({
 });
 
 // accessToken을 동적으로 헤더에 추가하는 인터셉터
-axiosPrivate.interceptors.request.use((config) => {
+axiosAuth.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token"); // 필요에 따라 수정
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
