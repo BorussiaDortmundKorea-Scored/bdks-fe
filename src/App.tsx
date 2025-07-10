16363496f5465f9cb9a36d76b3ae0f8479517015
youngduck <46455370+youngduck@users.noreply.github.com>
2025-07-10 16:30:42 +0900

    Bdks-f-1-초기세팅 (#1)
    * file: typescript lint옵션 erasableSyntaxOnly제거
    
    * feat: html favicon적용, title변경, lang 변경
    
    * chore: pr템플릿설정
    
    * feat: tailwind v4설치
    
    * chore: 환경 변수 설정
    
    * chore: ReactQuery provider 설정
    
    * chore: zustand 세팅
    
    * chore: husky 초기설정
    
    * chore: storybook v9설치
    
    * chore: 기본라우터설정
    
    * chore: vitest 세팅
    
    * chore: msw 설치
    
    * chore: 커서 룰 세팅
    
    * chore: changelog마크다운 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Router from "./shared/router/Router";
+
+export const queryClient = new QueryClient();
+
 function App() {
-  return <>어플리케이션</>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
