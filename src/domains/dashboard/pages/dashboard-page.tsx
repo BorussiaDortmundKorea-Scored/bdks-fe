8517d946dccbf0106dd69a4e10efdfe1440fac88
youngduck <dudejr5839@gmail.com>
2025-08-12 07:39:53 +0900

    feat: 컨테이너분리

--- src/domains/dashboard/pages/dashboard-page.tsx
+++ src/domains/dashboard/pages/dashboard-page.tsx
@@ -1,17 +1,19 @@
+import ReactQueryBoundary from "@shared/provider/react-query-boundary";
 import LogoutButton from "../../../shared/components/LogoutButton";
 import PlayerDb from "@dashboard/domains/player-db/player-db";
+import AnimalListError from "@animals/components/animal-list/error/animal-list-error";
 
 const DashboardPage = () => {
   return (
-    <div className="min-h-screen">
-      <div className="container mx-auto px-4 py-8">
-        {/* 헤더 영역 */}
-        <div className="mb-8 flex items-center justify-between">
-          <h1 className="text-3xl font-bold">축구 평점 시스템</h1>
-          <LogoutButton />
-        </div>
-        <PlayerDb />
+    <div className="containerabc">
+      {/* 헤더 영역 */}
+      <div className="mb-8 flex items-center justify-between">
+        <h1 className="text-3xl font-bold">축구 평점 시스템</h1>
+        <LogoutButton />
       </div>
+      <ReactQueryBoundary skeleton={<div>Loading...</div>} errorFallback={AnimalListError}>
+        <PlayerDb />
+      </ReactQueryBoundary>
     </div>
   );
 };
