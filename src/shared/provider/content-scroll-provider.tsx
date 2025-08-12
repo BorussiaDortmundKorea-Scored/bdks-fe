5d8667e5544f851fc46ae35da500e8f3fb3cd2b7
youngduck <dudejr5839@gmail.com>
2025-08-13 02:06:29 +0900

    feat: 가로스크롤

--- src/shared/provider/content-scroll-provider.tsx
+++ src/shared/provider/content-scroll-provider.tsx
@@ -6,7 +6,7 @@ interface ContentScrollProviderProps {
 }
 
 const ContentScrollProvider: React.FC<ContentScrollProviderProps> = ({ children, className = "" }) => {
-  return <div className={`scrollbar-hide overflow-y-auto ${className}`}>{children}</div>;
+  return <div className={`scrollbar-hide w-full overflow-y-auto ${className}`}>{children}</div>;
 };
 
 export default ContentScrollProvider;
