c13d9baccced3f84ba7dfd2f9366693ec5a804e7
youngduck <dudejr5839@naver.com>
2025-08-13 17:38:39 +0900

    feat: 스켈레톤 제작

--- src/domains/players/player-db/components/skeleton/player-db-skeleton.tsx
+++ src/domains/players/player-db/components/skeleton/player-db-skeleton.tsx
@@ -10,7 +10,7 @@ interface IPlayerDbSkeleton {}
 
 const PlayerDbSkeleton: React.FC<IPlayerDbSkeleton> = () => {
   //SECTION HOOK호출 영역
-
+  const skeletonItems = Array.from({ length: 5 }, (_, index) => index);
   //!SECTION HOOK호출 영역
 
   //SECTION 상태값 영역
@@ -23,7 +23,30 @@ const PlayerDbSkeleton: React.FC<IPlayerDbSkeleton> = () => {
 
   return (
     <PlayerDbWrapper>
-      <div data-testid="player-db-skeleton"></div>
+      <div data-testid="player-db-skeleton">
+        {/* 가로 스크롤 컨테이너 */}
+        <ul className="scrollbar-hide-x flex w-full flex-row gap-[8px] overflow-x-scroll">
+          {skeletonItems.map((index) => (
+            <li key={index} className="flex w-[105px] shrink-0 flex-col items-center gap-[16px]">
+              {/* 선수 이미지 skeleton */}
+              <div className="h-[80px] w-[80px] animate-pulse rounded bg-gray-600"></div>
+
+              {/* 선수 정보 skeleton */}
+              <div className="flex flex-col items-center gap-[2px]">
+                {/* 선수 이름 skeleton */}
+                <div className="h-[14px] w-[60px] animate-pulse rounded bg-gray-600"></div>
+
+                {/* 평점 정보 skeleton */}
+                <div className="flex items-center gap-[4px]">
+                  <div className="h-[12px] w-[25px] animate-pulse rounded bg-gray-600"></div>
+                  <span className="text-primary-100 text-xs">/</span>
+                  <div className="h-[12px] w-[25px] animate-pulse rounded bg-gray-600"></div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </PlayerDbWrapper>
   );
 };
