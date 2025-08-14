82a1bc0e638ff0c8d8b1bccd22ac1dc6057e13f9
youngduck <dudejr5839@naver.com>
2025-08-14 10:50:57 +0900

    refactor: 시간단위 상수모듈화

--- src/domains/players/player-db/api/react-query-api/use-get-player-db-with-my-ratings.tsx
+++ src/domains/players/player-db/api/react-query-api/use-get-player-db-with-my-ratings.tsx
@@ -1,11 +1,14 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { getPlayerDBWithMyRatings, type IPlayerDBWithMyRatings } from "../player-db-api";
 import { PLAYER_DB_QUERY_KEYS } from "./player-db-with-my-ratings-query-key";
+import { TIME_UNIT } from "@shared/constants/time-unit";
 
 export function useGetPlayerDBWithMyRatings(userId: string) {
   const { data } = useSuspenseQuery<IPlayerDBWithMyRatings[]>({
     queryKey: [PLAYER_DB_QUERY_KEYS.PLAYER_DB_WITH_MY_RATINGS, userId],
     queryFn: () => getPlayerDBWithMyRatings(userId),
+    staleTime: TIME_UNIT.ONE_MINUTE * 5,
+    gcTime: TIME_UNIT.ONE_MINUTE * 5,
   });
 
   return data;
