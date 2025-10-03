f821d75aba036d0bf9cb398a7a669555c51b5602
youngduck <dudejr5839@naver.com>
2025-10-04 01:17:15 +0900

    chore: react-helmet-async 라이브러리설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { HelmetProvider } from "react-helmet-async";
+
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import Router from "./shared/router/Router";
-import { queryClient } from "./shared/provider/query-client";
+
+import { queryClient } from "@shared/provider/query-client";
+import Router from "@shared/router/Router";
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 };
 
