6a65e5cba504586c369f64ed495d1916088cbc05
youngduck <dudejr5839@naver.com>
2025-10-20 10:24:33 +0900

    style: 평점입력페이지 선수이미지 크기축소

--- src/domains/matches/matches-lastest/matches-lastest-player-rating/pages/matches-lastest-player-rating-page.tsx
+++ src/domains/matches/matches-lastest/matches-lastest-player-rating/pages/matches-lastest-player-rating-page.tsx
@@ -3,11 +3,14 @@
  * 기능: 최근 경기 선수 평가 페이지 (경기중 + 다음경기 시작전까지 평점 입력 가능)
  * 프로세스 설명: 프로세스 복잡시 노션링크 첨부권장
  */
-import MatchesLastestPlayerRatingWrapper from "@matches/matches-lastest/matches-lastest-player-rating/components/wrapper/matches-lastest-player-rating-wrapper";
+import MatchesLastestPlayerRatingErrorFallback from "@matches/matches-lastest/matches-lastest-player-rating/components/error/matches-lastest-player-rating-error-fallback";
+import MatchesLastestPlayerRating from "@matches/matches-lastest/matches-lastest-player-rating/components/matches-lastest-player-rating";
+import MatchesLastestPlayerRatingSkeleton from "@matches/matches-lastest/matches-lastest-player-rating/components/skeleton/matches-lastest-player-rating-skeleton";
 
 import BackButton from "@shared/components/layout/header/buttons/back-button";
 import Header from "@shared/components/layout/header/header";
 import { usePageTransition } from "@shared/hooks/use-page-transition";
+import ReactQueryBoundary from "@shared/provider/react-query-boundary";
 
 //SECTION 리렌더링이 불필요한영역: 매직넘버, 문자열, 상수
 const options = {
@@ -31,7 +34,12 @@ const MatchesLastestPlayerRatingPage = () => {
   return (
     <div className="bdks-container" ref={pageRef}>
       <Header options={options} />
-      <MatchesLastestPlayerRatingWrapper />
+      <ReactQueryBoundary
+        skeleton={<MatchesLastestPlayerRatingSkeleton />}
+        errorFallback={MatchesLastestPlayerRatingErrorFallback}
+      >
+        <MatchesLastestPlayerRating />
+      </ReactQueryBoundary>
     </div>
   );
 };
