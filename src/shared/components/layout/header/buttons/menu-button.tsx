8c56c171fb05a3f3da91954e5b3e3477e805b062
youngduck <dudejr5839@naver.com>
2025-12-18 10:57:00 +0900

    feat: 대시보드페이지 헤더 단일로그아웃버튼에서 메뉴버튼으로 전환

--- src/shared/components/layout/header/buttons/menu-button.tsx
+++ src/shared/components/layout/header/buttons/menu-button.tsx
@@ -3,18 +3,50 @@
  * 기능:
  * 프로세스 설명: 프로세스 복잡시 노션링크 첨부권장
  */
-import { Menu } from "lucide-react";
+import { useState } from "react";
+
+import { Coffee, Menu } from "lucide-react";
+
+import { useAuth } from "@auth/contexts/AuthContext";
+
+import { LogoutButton } from "@shared/components/layout/header/buttons";
 
 const MenuButton = () => {
   //SECTION HOOK호출 영역
-
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const { user } = useAuth();
   //!SECTION HOOK호출 영역
 
   //SECTION 메서드 영역
+  const handleBlankNaverCafe = () => {
+    window.open("https://cafe.naver.com/greendkxc9", "_blank");
+  };
 
+  console.log("user", user);
   //!SECTION 메서드 영역
 
-  return <Menu size={24} className="text-primary-100 cursor-pointer" aria-label="메뉴" />;
+  return (
+    <>
+      <Menu
+        size={24}
+        className="text-primary-100 cursor-pointer"
+        aria-label="메뉴"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      />
+      {isMenuOpen && (
+        <div className="bg-background-primary fixed inset-0 top-15 z-10 h-19 w-full px-5 text-white">
+          <ul className="flex flex-col gap-4">
+            <li className="flex cursor-pointer items-center gap-2" onClick={handleBlankNaverCafe}>
+              <Coffee size={24} className="text-primary-100" /> 네이버 카페 바로가기
+            </li>
+            <li className="flex cursor-pointer items-center gap-2">
+              <LogoutButton /> 로그아웃 하기
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default MenuButton;
