b2af6111b50d9c3db07bbd7f84c5d7b63a8f26be
youngduck <dudejr5839@naver.com>
2026-08-18 14:35:11 +0900

    refactor: 404페이지 리디자인

--- src/shared/constants/supabse-storage.ts
+++ src/shared/constants/supabse-storage.ts
@@ -1,2 +1,3 @@
 export const SUPABASE_STORAGE_URL = `${import.meta.env.VITE_SUPABASE_URL}/storage/v1/object/public`;
 export const API_ERROR_LOGO = `${SUPABASE_STORAGE_URL}/asset/3d-icons/api-error.png`;
+export const LOGO_IMAGE = `${SUPABASE_STORAGE_URL}/asset/logo.png`;
